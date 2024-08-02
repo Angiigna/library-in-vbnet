b3ea9839521e2daac6eda068c9874ea2f4b11b87
Sivangi <sivangisankar19@gmail.com>
2024-08-02 09:29:11 +0530

    Add files via upload

---
+++ dbo.Studentdata.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Studentdata] (
+    [libraryid]      INT           NOT NULL,
+    [studentname]    NVARCHAR (50) NULL,
+    [registernumber] NVARCHAR (50) NULL,
+    [contactnumber]  NVARCHAR (50) NULL,
+    [noofbooks]      INT           NULL,
+    PRIMARY KEY CLUSTERED ([libraryid] ASC)
+);
+
